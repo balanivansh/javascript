6b0ecb3168e9d78c4ceae8975fa9c302ef689bff
balanivansh <86293861+balanivansh@users.noreply.github.com>
2024-03-04 13:56:14 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "vansh@gmail.com"
+var accountpwd = "12345"
+accountCity = "Bangalore"
+let accountbalance;
+
+// accountId = 2 // not allowed
+accountEmail = "hc@hc.com"
+accountpwd = "123"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountpwd, accountCity, accountbalance])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("hello")// we are using node js not browser
+
+let name = "vansh"
+let age = 18
+let isLoggedIn = true
+let state = null
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => stand alone value
+// undefined =>  
+// symbol => unique
+
+// object 
+
+console.log(typeof age);
+console.log(typeof null); // type- object
