c3f4ee1e8a0ffa41fa46cf8d3ea21f736f82e08c
balanivansh <86293861+balanivansh@users.noreply.github.com>
2024-03-17 09:45:39 +0000

    string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,28 @@
+const name = "vansh"
+const repoCount = 50
+
+// console.log(name + repoCount + " value");
+
+//modern way -> String Interpolation
+console.log(`Hello my name is ${name.toUpperCase()} amd my repo count is ${repoCount}`);
+
+// other way to declare string
+const gameName = new String('Vansh_vb')  // as a object, key:value pair
+// in this way we can use in-built functions
+
+console.log(gameName[0])
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase()); // original value is not changed
+console.log(gameName.charAt(2))
+console.log(gameName.indexOf('h'));
+
+console.log(gameName.split('_')) // converted to array
+// .substring(0,4)  // slicing
+// .slice(-8,4)  // -ve index allowed
+// .trim()
+// plzz try all string using and read mdn
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+console.log(url.replace('%20', '-'))
