28760abf79df10fc7a5f68fd14cdd4c3336eb143
balanivansh <86293861+balanivansh@users.noreply.github.com>
2024-03-17 08:05:00 +0000

    stack,heap

--- 01_basics/05_datatypes-summary.js
+++ 01_basics/05_datatypes-summary.js
@@ -1,15 +1,15 @@
-// Primitive 
+// Primitive    /// {Stack}
 // 7 types: String, Number, Boolean, null, undefined, symbol, bigint
 let nullEg = null ; //type = object
 let undefinedEg;
 const bigNum = 388348373678687468n
 console.log(typeof bigNum); // undefined
 
-// Non-premitive // Reference type
+// Non-premitive // Reference type   ///{Heap}
 // Array, Objects, Functions
 
 const heros = ["shaktiman","naagraj", "doga"] // array
-let muObj = { 
+let myObj = { 
     name:"vansh",
     age : 22
 }
@@ -18,3 +18,26 @@ const myFunction = function(){
 }
 
 // JS is dynamically typed
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack(primitive)
+// copy is created
+let myname = "javascript"
+let anothername = myname
+anothername = "react"
+
+console.log(myname); // copy of myname was given to anothername therefore original value didnot change
+console.log(anothername);
+
+
+// Heap(Non-primitive)
+// reference of original type ie changes take place in original value
+let user1 = {
+    email: "u1@gmail.com",
+    upi: "user@ybl"
+}
+let user2 = user1
+user2.email = "user2@gc"
+console.log(user1.email)
+console.log(user2.email); // both value are updated
